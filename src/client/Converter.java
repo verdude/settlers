a5416484be7db76f81b737fae8e971811e0f00a0
Santiago Verdu <santiago.verdu.01@gmail.com>
2016-01-22 10:15:57 -0700

    Made Javadoc-able

--- src/client/Converter.java
+++ src/client/Converter.java
@@ -7,8 +7,8 @@ public class Converter {
 
 
 	/**
-	 * @param jsonData
-	 * @throws FileNotFoundException
+	 * @param jsonData The json to de-serialize
+	 * @throws FileNotFoundException When the file is not found.
 	 * @pre None
 	 * @post The JSON data contained in the file is parsed and maps it to the model objects.
 	 */
@@ -17,7 +17,7 @@ public class Converter {
 	}
 	
 	/**
-	 * @param o
+	 * @param o The object to Serialize
 	 * @return The serialized model object
 	 * @pre None
 	 * @post The object serialized as JSON
