681a7ef1fc8eb291eed278f966b892387a869059
seanag0234 <Sean_George@Sean-Georges-MacBook-Pro.local>
2016-02-05 13:18:59 -0700

    added back VertexObject

---
+++ src/model/VertexObject.java
@@ -0,0 +1,21 @@
+package model;
+
+import shared.locations.VertexLocation;
+
+public class VertexObject {
+	int owner; // The index of the player that owns the vertex
+	VertexLocation location;
+	
+	
+	public VertexObject(){
+		//...
+	}
+	
+	// Getters
+	public int 	getOwner() 							{ return owner; 			}
+	public VertexLocation getLocation()					{ return location; 			}
+	
+	// Setters
+	public void setOwner(int owner) 					{ this.owner = owner; 		}
+	public void setLocation(VertexLocation location) 	{ this.location = location; }
+}
