9ffa9cd8184f67f2193e3d477ab68a074b141dfc
dro248 <david.ostler001@gmail.com>
2016-02-05 18:44:27 -0700

    additions to game map

--- src/clientTests/GameMapTest.java
+++ src/clientTests/GameMapTest.java
@@ -2,32 +2,45 @@ package clientTests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import shared.definitions.CatanColor;
 import model.*;
 
 public class GameMapTest {
+	static GameMap testMap;
 
 	@Test
+	public void init() {
+		testMap = new GameMap();
+		assertTrue(testMap.getHexList() 		!= null);
+		assertTrue(testMap.getPortList() 		!= null);
+		assertTrue(testMap.getRoadList() 		!= null);
+		assertTrue(testMap.getSettlementList() 	!= null);
+		assertTrue(testMap.getCityList() 		!= null);
+	}
+	
+	@Test
 	public void testMoveRobber() {
-//		test1 = new GameMap();
-//		assertTrue(test1.canRollNumber());
+		init();
+		
 	}
 	
 	@Test
 	public void testPlaceSettlement() {
-		
+		init();
 	}
 	
 	@Test
 	public void testPlaceCity() {
-		
+		init();
 	}
 	
 	@Test
 	public void testPlaceRoad() {
-		
+		init();
 	}
 
 }
