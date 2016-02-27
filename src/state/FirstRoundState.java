3fc2b9cb200d3ad75d4ac2cd8a91c04178e8c905
dro248 <david.ostler001@gmail.com>
2016-02-27 15:50:08 -0700

    Fixing serialization & model naming

--- src/state/FirstRoundState.java
+++ src/state/FirstRoundState.java
@@ -35,6 +35,15 @@ public class FirstRoundState implements IState {
 				return;
 			}
 			
+			
+			for(Hex i : hexList){
+//				HexType hexType = hexList[i];
+//				HexLocation hexLoc = new HexLocation(x, y);
+//				view.addHex(hexLoc, hexType);
+			}
+			
+			
+			/*
 			Random rand = new Random();
 			
 			for (int x = 0; x <= 3; ++x) {
@@ -52,7 +61,7 @@ public class FirstRoundState implements IState {
 					HexLocation hexLoc = new HexLocation(x, y);
 					view.addHex(hexLoc, hexType);
 					
-					/*				
+								
 	 				getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.NorthWest),
 							CatanColor.RED);
 					getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.SouthWest),
@@ -61,52 +70,53 @@ public class FirstRoundState implements IState {
 							CatanColor.ORANGE);
 					getView().placeSettlement(new VertexLocation(hexLoc, VertexDirection.NorthWest), CatanColor.GREEN);
 					getView().placeCity(new VertexLocation(hexLoc, VertexDirection.NorthEast), CatanColor.PURPLE);
-					*/
+					
 				}
 				
-				/*
-				if (x != 0) {
-					int minY = x - 3;
-					for (int y = minY; y <= 3; ++y) {
-						int r = rand.nextInt(HexType.values().length);
-						HexType hexType = HexType.values()[r];
-						HexLocation hexLoc = new HexLocation(-x, y);
-						getView().addHex(hexLoc, hexType);
-						getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.NorthWest),
-								CatanColor.RED);
-						getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.SouthWest),
-								CatanColor.BLUE);
-						getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.South),
-								CatanColor.ORANGE);
-						getView().placeSettlement(new VertexLocation(hexLoc, VertexDirection.NorthWest), CatanColor.GREEN);
-						getView().placeCity(new VertexLocation(hexLoc, VertexDirection.NorthEast), CatanColor.PURPLE);
-					}
-				}
-				*/
+				
+//				if (x != 0) {
+//					int minY = x - 3;
+//					for (int y = minY; y <= 3; ++y) {
+//						int r = rand.nextInt(HexType.values().length);
+//						HexType hexType = HexType.values()[r];
+//						HexLocation hexLoc = new HexLocation(-x, y);
+//						getView().addHex(hexLoc, hexType);
+//						getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.NorthWest),
+//								CatanColor.RED);
+//						getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.SouthWest),
+//								CatanColor.BLUE);
+//						getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.South),
+//								CatanColor.ORANGE);
+//						getView().placeSettlement(new VertexLocation(hexLoc, VertexDirection.NorthWest), CatanColor.GREEN);
+//						getView().placeCity(new VertexLocation(hexLoc, VertexDirection.NorthEast), CatanColor.PURPLE);
+//					}
+//				}
+				
 				
 			}
 
-			/*
-			PortType portType = PortType.BRICK;
-			getView().addPort(new EdgeLocation(new HexLocation(0, 3), EdgeDirection.North), portType);
-			getView().addPort(new EdgeLocation(new HexLocation(0, -3), EdgeDirection.South), portType);
-			getView().addPort(new EdgeLocation(new HexLocation(-3, 3), EdgeDirection.NorthEast), portType);
-			getView().addPort(new EdgeLocation(new HexLocation(-3, 0), EdgeDirection.SouthEast), portType);
-			getView().addPort(new EdgeLocation(new HexLocation(3, -3), EdgeDirection.SouthWest), portType);
-			getView().addPort(new EdgeLocation(new HexLocation(3, 0), EdgeDirection.NorthWest), portType);
 
-			getView().placeRobber(new HexLocation(0, 0));
-
-			getView().addNumber(new HexLocation(-2, 0), 2);
-			getView().addNumber(new HexLocation(-2, 1), 3);
-			getView().addNumber(new HexLocation(-2, 2), 4);
-			getView().addNumber(new HexLocation(-1, 0), 5);
-			getView().addNumber(new HexLocation(-1, 1), 6);
-			getView().addNumber(new HexLocation(1, -1), 8);
-			getView().addNumber(new HexLocation(1, 0), 9);
-			getView().addNumber(new HexLocation(2, -2), 10);
-			getView().addNumber(new HexLocation(2, -1), 11);
-			getView().addNumber(new HexLocation(2, 0), 12);
+//			PortType portType = PortType.BRICK;
+//			getView().addPort(new EdgeLocation(new HexLocation(0, 3), EdgeDirection.North), portType);
+//			getView().addPort(new EdgeLocation(new HexLocation(0, -3), EdgeDirection.South), portType);
+//			getView().addPort(new EdgeLocation(new HexLocation(-3, 3), EdgeDirection.NorthEast), portType);
+//			getView().addPort(new EdgeLocation(new HexLocation(-3, 0), EdgeDirection.SouthEast), portType);
+//			getView().addPort(new EdgeLocation(new HexLocation(3, -3), EdgeDirection.SouthWest), portType);
+//			getView().addPort(new EdgeLocation(new HexLocation(3, 0), EdgeDirection.NorthWest), portType);
+//
+//			getView().placeRobber(new HexLocation(0, 0));
+//
+//			getView().addNumber(new HexLocation(-2, 0), 2);
+//			getView().addNumber(new HexLocation(-2, 1), 3);
+//			getView().addNumber(new HexLocation(-2, 2), 4);
+//			getView().addNumber(new HexLocation(-1, 0), 5);
+//			getView().addNumber(new HexLocation(-1, 1), 6);
+//			getView().addNumber(new HexLocation(1, -1), 8);
+//			getView().addNumber(new HexLocation(1, 0), 9);
+//			getView().addNumber(new HexLocation(2, -2), 10);
+//			getView().addNumber(new HexLocation(2, -1), 11);
+//			getView().addNumber(new HexLocation(2, 0), 12);
+			
 			*/
 
 			//</temp>
