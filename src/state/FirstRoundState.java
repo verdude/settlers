f258f1cb1242ddaedf8db23c0862935f19795f38
dro248 <david.ostler001@gmail.com>
2016-02-27 15:15:05 -0700

    current status (DAVID)

--- src/state/FirstRoundState.java
+++ src/state/FirstRoundState.java
@@ -1,14 +1,20 @@
 package state;
 
-import client.data.RobPlayerInfo;
-import client.map.IMapView;
+import java.util.List;
+import java.util.Random;
+
 import model.ClientException;
 import model.ClientFacade;
+import model.GameMap;
+import model.Hex;
 import shared.definitions.CatanColor;
+import shared.definitions.HexType;
 import shared.definitions.PieceType;
 import shared.locations.EdgeLocation;
 import shared.locations.HexLocation;
 import shared.locations.VertexLocation;
+import client.data.RobPlayerInfo;
+import client.map.IMapView;
 
 /**
  * Created by Sean_George on 2/25/16.
@@ -17,7 +23,103 @@ public class FirstRoundState implements IState {
 
     @Override
     public void initFromModel(IMapView view) {
+    	// map init logic goes here!
+    	
+    	GameMap map;
+    	List<Hex> hexList;
+		try {
+			map = ClientFacade.getSingleton().getClientModel().getMap();
+			hexList = map.getHexList();
+			
+			if(hexList.size() < 1){
+				return;
+			}
+			
+			Random rand = new Random();
+			
+			for (int x = 0; x <= 3; ++x) {
 
+				int maxY = 3 - x;
+				for (int y = -3; y <= maxY; ++y) {
+					
+					System.out.println("X: " + x);
+					System.out.println("Y: " + y);
+					
+					int r = rand.nextInt(HexType.values().length);
+					System.out.println("HexType: " + HexType.values()[r]);
+					
+					HexType hexType = HexType.values()[r];
+					HexLocation hexLoc = new HexLocation(x, y);
+					view.addHex(hexLoc, hexType);
+					
+					/*				
+	 				getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.NorthWest),
+							CatanColor.RED);
+					getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.SouthWest),
+							CatanColor.BLUE);
+					getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.South),
+							CatanColor.ORANGE);
+					getView().placeSettlement(new VertexLocation(hexLoc, VertexDirection.NorthWest), CatanColor.GREEN);
+					getView().placeCity(new VertexLocation(hexLoc, VertexDirection.NorthEast), CatanColor.PURPLE);
+					*/
+				}
+				
+				/*
+				if (x != 0) {
+					int minY = x - 3;
+					for (int y = minY; y <= 3; ++y) {
+						int r = rand.nextInt(HexType.values().length);
+						HexType hexType = HexType.values()[r];
+						HexLocation hexLoc = new HexLocation(-x, y);
+						getView().addHex(hexLoc, hexType);
+						getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.NorthWest),
+								CatanColor.RED);
+						getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.SouthWest),
+								CatanColor.BLUE);
+						getView().placeRoad(new EdgeLocation(hexLoc, EdgeDirection.South),
+								CatanColor.ORANGE);
+						getView().placeSettlement(new VertexLocation(hexLoc, VertexDirection.NorthWest), CatanColor.GREEN);
+						getView().placeCity(new VertexLocation(hexLoc, VertexDirection.NorthEast), CatanColor.PURPLE);
+					}
+				}
+				*/
+				
+			}
+
+			/*
+			PortType portType = PortType.BRICK;
+			getView().addPort(new EdgeLocation(new HexLocation(0, 3), EdgeDirection.North), portType);
+			getView().addPort(new EdgeLocation(new HexLocation(0, -3), EdgeDirection.South), portType);
+			getView().addPort(new EdgeLocation(new HexLocation(-3, 3), EdgeDirection.NorthEast), portType);
+			getView().addPort(new EdgeLocation(new HexLocation(-3, 0), EdgeDirection.SouthEast), portType);
+			getView().addPort(new EdgeLocation(new HexLocation(3, -3), EdgeDirection.SouthWest), portType);
+			getView().addPort(new EdgeLocation(new HexLocation(3, 0), EdgeDirection.NorthWest), portType);
+
+			getView().placeRobber(new HexLocation(0, 0));
+
+			getView().addNumber(new HexLocation(-2, 0), 2);
+			getView().addNumber(new HexLocation(-2, 1), 3);
+			getView().addNumber(new HexLocation(-2, 2), 4);
+			getView().addNumber(new HexLocation(-1, 0), 5);
+			getView().addNumber(new HexLocation(-1, 1), 6);
+			getView().addNumber(new HexLocation(1, -1), 8);
+			getView().addNumber(new HexLocation(1, 0), 9);
+			getView().addNumber(new HexLocation(2, -2), 10);
+			getView().addNumber(new HexLocation(2, -1), 11);
+			getView().addNumber(new HexLocation(2, 0), 12);
+			*/
+
+			//</temp>
+			
+			
+			
+			
+		} catch (ClientException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	
     }
 
     @Override
