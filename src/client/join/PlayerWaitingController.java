d7e55845795af74eab46c82288605a4002ba5525
S. Jacob Powell <s.jacob.powell@gmail.com>
2016-02-26 20:01:08 -0700

    DON'T MESS IT UP. This should be working.

--- src/client/join/PlayerWaitingController.java
+++ src/client/join/PlayerWaitingController.java
@@ -39,31 +39,26 @@ public class PlayerWaitingController extends Controller implements IPlayerWaitin
 		getView().showModal();
 		System.out.println("in the waiting controller");
 		Timer timer = new Timer();
-		GameInfo[] games;
-		try {
-			games = Converter.deserializeGamesArray(ClientFacade.getSingleton().gamesList());
-
-		} catch (ClientException e) {
-			System.out.println("Error getting game list");
-			e.printStackTrace();
-			return;
-		}
 		timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                for (GameInfo game : games) {
-                    try {
-                        if (game.getId() == Integer.parseInt(ServerProxy.getSingleton().getGameID())) {
-                            if (game.getPlayers().size() == 4) {
-                                getView().closeModal();
-                                timer.cancel();
-                                timer.purge();
-                            }
-                            break;
-                        }
-                    } catch (ClientException e) {
-                        e.printStackTrace();
-                    }
+        		GameInfo[] games;
+        		try {
+        			games = Converter.deserializeGamesArray(ClientFacade.getSingleton().gamesList());
+
+	                for (GameInfo game : games) {
+	                        if (game.getId() == Integer.parseInt(ServerProxy.getSingleton().getGameID())) {
+	                            if (game.getPlayers().size() == 4) {
+	                                getView().closeModal();
+	                                this.cancel();
+	                            }
+	                            break;
+	                        }
+	                }
+        		} catch (ClientException e) {
+        			System.out.println("Error getting game list");
+        			e.printStackTrace();
+        			return;
                 }
             }
         }, 0, 3000);
