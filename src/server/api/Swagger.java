fb58bc223f739200deaa6fe11b937164d64f53e7
S. Jacob Powell <s.jacob.powell@gmail.com>
2016-03-19 13:38:44 -0600

    Swagger page final works. I've finished all my parts so let me know if you have issues

---
+++ src/server/api/Swagger.java
@@ -0,0 +1,35 @@
+package server.api;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+@Path("")
+public class Swagger {
+	@GET
+	@Path("docs/api/data")
+	@Produces(MediaType.APPLICATION_JSON)
+	public InputStream getJsonFile() throws FileNotFoundException
+	{
+		return new FileInputStream("docs/api/data.json");
+	}
+	@GET
+	@Path("{path:.*}")
+	public InputStream viewSwagger(@PathParam("path") String path) throws FileNotFoundException
+	{
+		System.out.println(path);
+		try {
+			if(path.isEmpty()) {
+				throw new FileNotFoundException();
+			}
+			return new FileInputStream(path);
+		} catch (FileNotFoundException e) {
+			return new FileInputStream("docs/api/view/index.html");
+		}
+	}
+}
