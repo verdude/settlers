cfae454454c0fa85f2c4cd7e89a70218b0ab951d
seanag0234 <Sean_George@byu592535wks.app.byu.edu>
2016-04-06 12:15:32 -0600

    Added getUserDAO and getGameDAO to Factory

--- src/server/Factory.java
+++ src/server/Factory.java
@@ -1,29 +1,31 @@
 package server;
 
-import server.database.IDAO;
+import server.database.GameDAO;
+import server.database.UserDAO;
 
 public class Factory implements PeristenceProvider{
 
-	private  IDAO currentDAO;
+	private  GameDAO gameDAO;
+	private  UserDAO userDAO;
 
 
-	@Override
-	public  IDAO getDAO(String db_type, String jar_filename){
-
-		switch (db_type) {
-			case "sql":
-				//currentDAO = (IDAO) new SqlDAO(jar_filename);
-				return currentDAO;
-			case "ini":
-				//currentDAO = (IDAO) new IniDAO(jar_filename);
-				return currentDAO;
-			default:
-				System.out.println("ERROR: Invalid databaseType!");
-				System.out.println("Valid Types: \"sql\" | \"ini\" ");
-				break;
-		}
-		return null;
-	}
+//	@Override
+//	public  IDAO getDAO(String db_type, String jar_filename){
+//
+//		switch (db_type) {
+//			case "sql":
+//				//currentDAO = (IDAO) new SqlDAO(jar_filename);
+//				return currentDAO;
+//			case "ini":
+//				//currentDAO = (IDAO) new IniDAO(jar_filename);
+//				return currentDAO;
+//			default:
+//				System.out.println("ERROR: Invalid databaseType!");
+//				System.out.println("Valid Types: \"sql\" | \"ini\" ");
+//				break;
+//		}
+//		return null;
+//	}
 
 
 	@Override
@@ -37,8 +39,18 @@ public class Factory implements PeristenceProvider{
 	}
 
 	@Override
-	public void clearPersistance() {
+	public void clearPersistence() {
 		
+	}
+
+	@Override
+	public UserDAO getUserDAO() {
+		return userDAO;
+	}
+
+	@Override
+	public GameDAO getGameDAO() {
+		return gameDAO;
 	}
 
 
--- src/server/PeristenceProvider.java
+++ src/server/PeristenceProvider.java
@@ -1,20 +1,21 @@
 package server;
 
-import server.database.IDAO;
+import server.database.GameDAO;
+import server.database.UserDAO;
 
 /**
  * Created by Sean_George on 4/4/16.
  */
 public interface PeristenceProvider {
 
-    /**
-     * @param db_type The command line argument that determines what kind of persistence system will be used.
-     * @param jar_filename The path to the jar file
-     * @return The proper DAO as given by the command line argument.
-     * @pre None
-     * @post The proper DAO as given by the command line argument.
-     */
-    public  IDAO getDAO(String db_type, String jar_filename);
+//    /**
+//     * @param db_type The command line argument that determines what kind of persistence system will be used.
+//     * @param jar_filename The path to the jar file
+//     * @return The proper DAO as given by the command line argument.
+//     * @pre None
+//     * @post The proper DAO as given by the command line argument.
+//     */
+//    public  IDAO getDAO(String db_type, String jar_filename);
 
     /**
      * Begins a transaction on the database
@@ -34,5 +35,10 @@ public interface PeristenceProvider {
     /**
      * Clears whatever plugin is currently being used back to a default state.
      */
-    public void clearPersistance();
+    public void clearPersistence();
+
+    public UserDAO getUserDAO();
+
+    public GameDAO getGameDAO();
+
 }
